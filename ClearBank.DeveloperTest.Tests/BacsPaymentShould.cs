a6eca76e511d559f6bbaf7ce94a8e0be7ae5f589
Savaş Cilve <savascilve@outlook.com>
2019-02-19 20:48:51 +0000

    Added tests for bacs payment.

---
+++ ClearBank.DeveloperTest.Tests/BacsPaymentShould.cs
@@ -0,0 +1,62 @@
+﻿using ClearBank.DeveloperTest.Services.PaymentSchemes;
+using ClearBank.DeveloperTest.Types;
+using Moq;
+using NUnit.Framework;
+
+namespace ClearBank.DeveloperTest.Tests
+{
+    [TestFixture]
+    public class BacsPaymentShould
+    {
+        private BacsPayment _bacsPayment;
+        private Account _account;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _bacsPayment = new BacsPayment();
+            _account = new Account();
+        }
+
+        [Test]
+        public void ReturnTrue_WhenIsMatchCalledWithBacsScheme()
+        {
+            var result = _bacsPayment.IsMatch(PaymentScheme.Bacs);
+
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void ReturnUnsuccessfulResult_WhenAccountIsNull()
+        {
+            _account = null;
+
+            var result = _bacsPayment.Process(_account, It.IsAny<decimal>());
+
+            Assert.IsNotNull(result);
+            Assert.IsFalse(result.Success);
+        }
+
+        [Test]
+        public void ReturnUnsuccessfulResult_WhenAccountHasChapsFlag()
+        {
+            _account.AllowedPaymentSchemes = AllowedPaymentSchemes.Chaps;
+
+            var result = _bacsPayment.Process(_account, It.IsAny<decimal>());
+
+            Assert.IsNotNull(result);
+            Assert.IsFalse(result.Success);
+        }
+
+        [Test]
+        public void ReturnSuccessfulResult_WhenAccountIsValid()
+        {
+            _account.AllowedPaymentSchemes = AllowedPaymentSchemes.Bacs;
+
+            var result = _bacsPayment.Process(_account, It.IsAny<decimal>());
+
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.Success);
+        }
+    }
+}
