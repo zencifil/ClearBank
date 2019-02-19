13f17f8759c80ae113d2416027e3fa9303f39176
Savaş Cilve <savascilve@outlook.com>
2019-02-19 20:54:20 +0000

    Added tests for faster payment.

---
+++ ClearBank.DeveloperTest.Tests/FasterPaymentShould.cs
@@ -0,0 +1,74 @@
+﻿using ClearBank.DeveloperTest.Services.PaymentSchemes;
+using ClearBank.DeveloperTest.Types;
+using Moq;
+using NUnit.Framework;
+
+namespace ClearBank.DeveloperTest.Tests
+{
+    [TestFixture]
+    public class FasterPaymentShould
+    {
+        private FasterPayment _fasterPayment;
+        private Account _account;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _fasterPayment = new FasterPayment();
+            _account = new Account();
+        }
+
+        [Test]
+        public void ReturnTrue_WhenIsMatchCalledWithFasterPaymentScheme()
+        {
+            var result = _fasterPayment.IsMatch(PaymentScheme.FasterPayments);
+
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void ReturnUnsuccessfulResult_WhenAccountIsNull()
+        {
+            _account = null;
+
+            var result = _fasterPayment.Process(_account, It.IsAny<decimal>());
+
+            Assert.IsNotNull(result);
+            Assert.IsFalse(result.Success);
+        }
+
+        [Test]
+        public void ReturnUnsuccessfulResult_WhenAccountHasBacsFlag()
+        {
+            _account.AllowedPaymentSchemes = AllowedPaymentSchemes.Bacs;
+
+            var result = _fasterPayment.Process(_account, It.IsAny<decimal>());
+
+            Assert.IsNotNull(result);
+            Assert.IsFalse(result.Success);
+        }
+
+        [Test]
+        public void ReturnUnsuccessfulResult_WhenAccountHasInsufficientFunds()
+        {
+            _account.AllowedPaymentSchemes = AllowedPaymentSchemes.FasterPayments;
+            _account.Balance = 0M;
+
+            var result = _fasterPayment.Process(_account, 10M);
+
+            Assert.IsNotNull(result);
+            Assert.IsFalse(result.Success);
+        }
+
+        [Test]
+        public void ReturnSuccessfulResult_WhenAccountIsValid()
+        {
+            _account.AllowedPaymentSchemes = AllowedPaymentSchemes.FasterPayments;
+
+            var result = _fasterPayment.Process(_account, It.IsAny<decimal>());
+
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.Success);
+        }
+    }
+}
