f8b7397ec8103d4490451dd7bf392df666b2515d
Savaş Cilve <savascilve@outlook.com>
2019-02-19 20:57:26 +0000

    Added tests for balance and payment service.

---
+++ ClearBank.DeveloperTest.Tests/BalanceServiceShould.cs
@@ -0,0 +1,34 @@
+﻿using ClearBank.DeveloperTest.Data;
+using ClearBank.DeveloperTest.Services;
+using ClearBank.DeveloperTest.Types;
+using Moq;
+using NUnit.Framework;
+
+namespace ClearBank.DeveloperTest.Tests
+{
+    [TestFixture]
+    public class BalanceServiceShould
+    {
+        private BalanceService _balanceService;
+        private Mock<IAccountDataStore> _accountDataStore;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _accountDataStore = new Mock<IAccountDataStore>();
+            _accountDataStore.Setup(ads => ads.UpdateAccount(It.IsAny<Account>()));
+
+            _balanceService = new BalanceService(_accountDataStore.Object);
+        }
+
+        [Test]
+        public void DeductAmountFromAccountBalance_WhenDeductBalanceIsCalled()
+        {
+            var account = new Account { Balance = 100M };
+
+            _balanceService.DeductBalance(account, 20M);
+
+            Assert.AreEqual(80M, account.Balance);
+        }
+    }
+}
