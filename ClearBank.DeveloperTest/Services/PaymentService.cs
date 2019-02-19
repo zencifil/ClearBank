ca99e5ffab3f023f36522fcb3e30aa04bcf1be6f
zencifil <savascilve@outlook.com>
2019-02-19 23:09:08 +0000

    Added make payment request validator.

--- ClearBank.DeveloperTest/Services/PaymentService.cs
+++ ClearBank.DeveloperTest/Services/PaymentService.cs
@@ -3,18 +3,22 @@ using System.Linq;
 using ClearBank.DeveloperTest.Data;
 using ClearBank.DeveloperTest.Services.PaymentSchemes;
 using ClearBank.DeveloperTest.Types;
+using FluentValidation;
 
 namespace ClearBank.DeveloperTest.Services
 {
     public class PaymentService : IPaymentService
     {
+        private readonly IValidator _validator;
         private readonly IAccountDataStore _accountDataStore;
         private readonly IBalanceService _balanceService;
         private readonly List<IPaymentScheme> _paymentSchemes;
 
-        public PaymentService(IAccountDataStore accountDataStore,
+        public PaymentService(IValidator validator,
+            IAccountDataStore accountDataStore,
             IBalanceService balanceService)
         {
+            _validator = validator;
             _accountDataStore = accountDataStore;
             _balanceService = balanceService;
 
@@ -28,6 +32,10 @@ namespace ClearBank.DeveloperTest.Services
 
         public MakePaymentResult MakePayment(MakePaymentRequest request)
         {
+            var validationResult = _validator.Validate(request);
+            if (validationResult.Errors.Any())
+                return new MakePaymentResult { Success = false };
+
             var account = _accountDataStore.GetAccount(request.DebtorAccountNumber);
             var result = GetPaymentScheme(request).Process(account, request.Amount);
 
---
+++ ClearBank.DeveloperTest/Validators/MakePaymentRequestValidator.cs
@@ -0,0 +1,16 @@
+﻿using ClearBank.DeveloperTest.Types;
+using FluentValidation;
+
+namespace ClearBank.DeveloperTest.Validators
+{
+    public class MakePaymentRequestValidator : AbstractValidator<MakePaymentRequest>
+    {
+        public MakePaymentRequestValidator()
+        {
+            RuleFor(r => r).NotNull();
+            RuleFor(r => r.DebtorAccountNumber).NotEmpty();
+            RuleFor(r => r.CreditorAccountNumber).NotEmpty();
+            RuleFor(r => r.Amount).GreaterThan(0M);
+        }
+    }
+}
