77f33469711c14e25a64b703b8974c3c0e12a7da
Savaş Cilve <savascilve@outlook.com>
2019-02-19 20:38:19 +0000

    Implemented payment scheme to payment service.

--- ClearBank.DeveloperTest/Services/PaymentService.cs
+++ ClearBank.DeveloperTest/Services/PaymentService.cs
@@ -1,4 +1,7 @@
-﻿using ClearBank.DeveloperTest.Data;
+﻿using System.Collections.Generic;
+using System.Linq;
+using ClearBank.DeveloperTest.Data;
+using ClearBank.DeveloperTest.Services.PaymentSchemes;
 using ClearBank.DeveloperTest.Types;
 
 namespace ClearBank.DeveloperTest.Services
@@ -6,61 +9,24 @@ namespace ClearBank.DeveloperTest.Services
     public class PaymentService : IPaymentService
     {
         private readonly IAccountDataStore _accountDataStore;
+        private readonly List<IPaymentScheme> _paymentSchemes;
 
         public PaymentService(IAccountDataStore accountDataStore)
         {
             _accountDataStore = accountDataStore;
+
+            _paymentSchemes = new List<IPaymentScheme>
+            {
+                new BacsPayment(),
+                new ChapsPayment(),
+                new FasterPayment()
+            };
         }
 
         public MakePaymentResult MakePayment(MakePaymentRequest request)
         {
             var account = _accountDataStore.GetAccount(request.DebtorAccountNumber);
-
-            var result = new MakePaymentResult();
-
-            switch (request.PaymentScheme)
-            {
-                case PaymentScheme.Bacs:
-                    if (account == null)
-                    {
-                        result.Success = false;
-                    }
-                    else if (!account.AllowedPaymentSchemes.HasFlag(AllowedPaymentSchemes.Bacs))
-                    {
-                        result.Success = false;
-                    }
-                    break;
-
-                case PaymentScheme.FasterPayments:
-                    if (account == null)
-                    {
-                        result.Success = false;
-                    }
-                    else if (!account.AllowedPaymentSchemes.HasFlag(AllowedPaymentSchemes.FasterPayments))
-                    {
-                        result.Success = false;
-                    }
-                    else if (account.Balance < request.Amount)
-                    {
-                        result.Success = false;
-                    }
-                    break;
-
-                case PaymentScheme.Chaps:
-                    if (account == null)
-                    {
-                        result.Success = false;
-                    }
-                    else if (!account.AllowedPaymentSchemes.HasFlag(AllowedPaymentSchemes.Chaps))
-                    {
-                        result.Success = false;
-                    }
-                    else if (account.Status != AccountStatus.Live)
-                    {
-                        result.Success = false;
-                    }
-                    break;
-            }
+            var result = GetPaymentScheme(request).Process(account, request.Amount);
 
             if (result.Success)
             {
@@ -69,6 +35,11 @@ namespace ClearBank.DeveloperTest.Services
             }
 
             return result;
+        }
+
+        private IPaymentScheme GetPaymentScheme(MakePaymentRequest request)
+        {
+            return _paymentSchemes.Single(scheme => scheme.IsMatch(request.PaymentScheme));
         }
     }
 }
