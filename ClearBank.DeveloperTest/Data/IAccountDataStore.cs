05be334b064a3934b8a5972cae13d277b9d15893
Savaş Cilve <savascilve@outlook.com>
2019-02-19 20:08:24 +0000

    Introduced IAccountDataStore for account data store abstraction.

---
+++ ClearBank.DeveloperTest/Data/IAccountDataStore.cs
@@ -0,0 +1,10 @@
+﻿using ClearBank.DeveloperTest.Types;
+
+namespace ClearBank.DeveloperTest.Data
+{
+    public interface IAccountDataStore
+    {
+        Account GetAccount(string accountNumber);
+        void UpdateAccount(Account account);
+    }
+}
