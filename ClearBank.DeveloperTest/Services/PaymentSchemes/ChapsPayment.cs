4b726130da6f179c36aac532864c3aaaed5596a5
Savaş Cilve <savascilve@outlook.com>
2019-02-19 20:35:28 +0000

    Added payment schemes.

---
+++ ClearBank.DeveloperTest/Services/PaymentSchemes/ChapsPayment.cs
@@ -0,0 +1,20 @@
+﻿using ClearBank.DeveloperTest.Types;
+
+namespace ClearBank.DeveloperTest.Services.PaymentSchemes
+{
+    public class ChapsPayment : IPaymentScheme
+    {
+        public bool IsMatch(PaymentScheme paymentScheme)
+        {
+            return paymentScheme == PaymentScheme.Chaps;
+        }
+
+        public MakePaymentResult Process(Account account, decimal amount)
+        {
+            if (account == null || !account.AllowedPaymentSchemes.HasFlag(AllowedPaymentSchemes.Chaps) || account.Status != AccountStatus.Live)
+                return new MakePaymentResult { Success = false };
+
+            return new MakePaymentResult { Success = true };
+        }
+    }
+}
